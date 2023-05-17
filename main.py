a18034e58a5c3956e962abd2bcca973039d509c2
yunko006 <peyrachethomas@gmail.com>
2023-05-18 00:37:05 +0200

    Add: exceptions / rewrite main logic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 import yagmail
+# from yagmail.error import SMTPSenderRefused
+import smtplib
 from utils import get_epub_file, move_file
 
 
@@ -8,25 +10,33 @@ load_dotenv()
 
 SENDER = os.getenv("gmail_username")
 PASSWORD = os.getenv("gmail_password")
-RECEIVER = os.getenv("gmail_username")
+RECEIVER = os.getenv("email_receiver")
 
-def send_email(str_input):
+def send_email(files):
     body = ""
-    # filename = "document.pdf"
-    files = get_epub_file(str_input)
 
-    yag = yagmail.SMTP(SENDER, PASSWORD)
-    yag.send(
-        to=RECEIVER,
-        subject="Livres",
-        contents=body, 
-        attachments=[file for file in files],
-    )
+    try:
+        yag = yagmail.SMTP(SENDER, PASSWORD)
+        yag.send(
+            to=RECEIVER,
+            subject="Livres",
+            contents=body, 
+            attachments=[file for file in files],
+        )
+        print("l'email a bien été envoyé.")
+
+    except smtplib.SMTPException:
+        print("fichier trop volumineux")
+    
+
+    except Exception as e:
+        print(f'erreur: {e}')
 
 
 if __name__ == "__main__":
     str_input = str(input('Path : ["telegram" ou "telechargement]: '))
-    send_email(str_input)
+    files = get_epub_file(str_input)
+    send_email(files)
     print('email envoyé.')
-    move_file(str_input)
+    move_file(files)
     print("terminé.")
