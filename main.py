57fb2e5af3f496bff8955fd5deb5b96d2375ba74
yunko006 <peyrachethomas@gmail.com>
2023-08-08 21:54:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import yagmail
+
 # from yagmail.error import SMTPSenderRefused
 import smtplib
 from utils import get_epub_file, move_file
@@ -12,6 +13,7 @@ SENDER = os.getenv("gmail_username")
 PASSWORD = os.getenv("gmail_password")
 RECEIVER = os.getenv("email_receiver")
 
+
 def send_email(files):
     body = ""
 
@@ -19,24 +21,23 @@ def send_email(files):
         yag = yagmail.SMTP(SENDER, PASSWORD)
         yag.send(
             to=RECEIVER,
-            subject="Livres",
-            contents=body, 
+            subject="Livresss",
+            contents=body,
             attachments=[file for file in files],
         )
         print("l'email a bien été envoyé.")
 
     except smtplib.SMTPException:
         print("fichier trop volumineux")
-    
 
     except Exception as e:
-        print(f'erreur: {e}')
+        print(f"erreur: {e}")
 
 
 if __name__ == "__main__":
     str_input = str(input('Path : ["telegram" ou "telechargement]: '))
     files = get_epub_file(str_input)
     send_email(files)
-    print('email envoyé.')
+    print("email envoyé.")
     move_file(files)
     print("terminé.")
